e8cc085c1de8104ef64c31b558e446a6078e52b9
Mena Erian <mena3koko2karas1@gmail.com>
2025-08-05 23:02:47 +0300

    Phone Book

--- Lab/Program.cs
+++ Lab/Program.cs
@@ -1,4 +1,5 @@
-﻿using Lab.MyZoo;
+﻿using Lab.MyPhoneBook;
+using Lab.MyZoo;
 using Lab.MyZoo.Animals;
 using System.Collections;
 
@@ -25,6 +26,32 @@ namespace Lab
             /// {
             ///     Console.WriteLine(animal);
             /// } 
+            #endregion
+
+            #region Phone Book
+            //PhoneBook phoneBook = new PhoneBook()
+            //{
+            //    new PhoneBook.Person(){ Name ="Mena",Number=01200523080 },
+            //    new PhoneBook.Person(){ Name ="fds",Number=01278596432 },
+            //    new PhoneBook.Person(){ Name ="sfd",Number=01200523080 },
+            //    new PhoneBook.Person(){ Name ="Hosam",Number=01200564805 },
+            //    new PhoneBook.Person(){ Name ="we",Number=01200523080 },
+            //    new PhoneBook.Person(){ Name ="MfsS",Number=01200512350 },
+            //    new PhoneBook.Person(){ Name ="Ali",Number=01200000000 },
+            //    new PhoneBook.Person(){ Name ="Msdfna",Number=01234565870 },
+            //    new PhoneBook.Person(){ Name ="MeFsd",Number=0564981488 },
+            //};
+
+            //foreach (PhoneBook.Person person in phoneBook)
+            //    Console.WriteLine(person);
+            ////phoneBook[01200564805] = "Ali";
+            ////Console.WriteLine(phoneBook[(long)01200564805]);
+            //phoneBook["Hosam"] = 456;
+
+            //Console.WriteLine(phoneBook["Ali"]);
+            //int phone = phoneBook["Ali"];
+
+            //Console.WriteLine(phone); 
             #endregion
         }
     }
